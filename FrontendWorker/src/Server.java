2a1b3cb3e829e5241a0ede6e15e1f453541b5e67
enzo <enzo.piccolino@gmail.com>
2012-10-21 23:32:16 +0200

    clean up all warning

--- FrontendWorker/src/Server.java
+++ FrontendWorker/src/Server.java
@@ -1,20 +1,9 @@
 import java.io.*;
 import com.amazonaws.auth.AWSCredentials;
-import java.net.*;
 import org.jdom2.*;
 import org.jdom2.output.*;
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.PropertiesCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.ListObjectsRequest;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.ObjectListing;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
+
 
 
 
