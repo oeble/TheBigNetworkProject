bcbc366f42ed04dd433d817b29357d31fec22c12
ondraco <ondraco@gmail.com>
2012-10-22 02:38:42 +0200

    added AutoMagnetoHyperionTransformatorKillAllHumans function

--- FrontendWorker/src/DDBReader.java
+++ FrontendWorker/src/DDBReader.java
@@ -18,6 +18,7 @@ import com.amazonaws.services.dynamodb.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodb.model.AttributeValue;
 import com.amazonaws.services.dynamodb.model.ComparisonOperator;
 import com.amazonaws.services.dynamodb.model.Condition;
+import com.amazonaws.services.dynamodb.model.Key;
 import com.amazonaws.services.dynamodb.model.QueryRequest;
 import com.amazonaws.services.dynamodb.model.QueryResult;
 
@@ -63,56 +64,73 @@ public class DDBReader {
 			return RequestID.createError("XMLError", end, "Impossible to parse this date" + e.getMessage()).toString();
 		}
 
-	
 		// Making request
 		Condition condition = new Condition().withComparisonOperator(ComparisonOperator.BETWEEN)
-				.withAttributeValueList(
-						 new AttributeValue().withN(tbegin.toString() + "000"), new AttributeValue().withN(tend.toString() + "000"));
+				.withAttributeValueList(new AttributeValue().withN(tbegin.toString() + "000"),
+						new AttributeValue().withN(tend.toString() + "000"));
 
-		QueryRequest queryRequest = new QueryRequest().withTableName(tableName)
-				.withHashKeyValue(new AttributeValue().withN(cellid.toString())).withRangeKeyCondition(condition)
-				.withAttributesToGet(Arrays.asList("timestamp", "carType", "data"));
-
-		QueryResult result = dynamoDB.query(queryRequest);
-		LOGGER.log(Level.INFO, "Result from request : " + result);
-
-		// Parsing answer
-
-		String numberOfData = result.getCount().toString();
-		String firstType = result.getItems().get(0).get("carType").getN();
-		String firstTime = result.getItems().get(0).get("timestamp").getN();
-
-		// remove the 3 last digit (nodeid)
-		firstTime = firstTime.substring(0, firstTime.length() - 4);
-
-		firstTime = dateFormatter.format(new Date(Long.parseLong(firstTime)));
-		String lastType = result.getItems().get(result.getCount() - 1).get("carType").getN();
-		String lastTime = result.getItems().get(result.getCount() - 1).get("timestamp").getN();
-
-		// remove the 3 last digit (nodeid)
-		lastTime = firstTime.substring(0, lastTime.length() - 4);
-
-		lastTime = dateFormatter.format(new Date(Long.parseLong(lastTime)));
+		Key lastKeyEvaluated = null;
 		Long rawDataSize = 0L;
-		for (Map<String, AttributeValue> item : result.getItems()) {
-			rawDataSize += item.get("data").getB().array().length;
-		}
+		int numberofdata = 0;
+		
+		String firstType = "",firstTime = "",lastType = "",lastTime = ""; 
+		do {
+			QueryRequest queryRequest = new QueryRequest().withTableName(tableName)
+					.withHashKeyValue(new AttributeValue().withN(cellid.toString())).withRangeKeyCondition(condition)
+					.withAttributesToGet(Arrays.asList("timestamp", "carType", "data")).withExclusiveStartKey(lastKeyEvaluated);
+
+			QueryResult result = dynamoDB.query(queryRequest);
+			LOGGER.log(Level.INFO, "Result from request : " + result);
+			lastKeyEvaluated = result.getLastEvaluatedKey();
+			// Parsing answer
+
+			if(numberofdata == 0){
+			firstType = result.getItems().get(0).get("carType").getN();
+			firstTime = result.getItems().get(0).get("timestamp").getN();
+			// remove the 3 last digit (nodeid)
+			firstTime = firstTime.substring(0, firstTime.length() - 4);
+			firstTime = dateFormatter.format(new Date(Long.parseLong(firstTime)));
+			}
+			
+			numberofdata += result.getCount();
+			
+			if(lastKeyEvaluated == null){
+			lastType = result.getItems().get(result.getCount() - 1).get("carType").getN();
+			lastTime = result.getItems().get(result.getCount() - 1).get("timestamp").getN();
+			
+			// remove the 3 last digit (nodeid)
+			lastTime = lastTime.substring(0, lastTime.length() - 4);
+			lastTime = dateFormatter.format(new Date(Long.parseLong(lastTime)));
+			}
+
+			for (Map<String, AttributeValue> item : result.getItems()) {
+				rawDataSize += item.get("data").getB().array().length;
+			}
+		} while (lastKeyEvaluated != null);
+
 		return RequestID.createReplyCellStatNet(reqId, cellid, begin, end, firstType, firstTime, lastType, lastTime,
-				numberOfData, String.valueOf((rawDataSize / 1024) / 1024)).toString();
+				String.valueOf(numberofdata), String.valueOf((rawDataSize / 1024) / 1024)).toString();
 	}
 
 	// list of cells
 	public String reqListCells(String reqId) {
 
 		// hack for the cellIds
-		QueryRequest queryRequest = new QueryRequest().withTableName(tableName)
-				.withHashKeyValue(new AttributeValue().withN("-1")).withAttributesToGet(Arrays.asList("timestamp"));
-		QueryResult result = dynamoDB.query(queryRequest);
-
 		List<String> cellIdsList = new LinkedList<String>();
-		for (Map<String, AttributeValue> item : result.getItems()) {
-			cellIdsList.add(item.get("timestamp").getN());
-		}
+		Key lastKeyEvaluated = null;
+		do {
+			QueryRequest queryRequest = new QueryRequest().withTableName(tableName)
+					.withHashKeyValue(new AttributeValue().withN("-1")).withAttributesToGet(Arrays.asList("timestamp"))
+					.withExclusiveStartKey(lastKeyEvaluated);
+			QueryResult result = dynamoDB.query(queryRequest);
+
+			for (Map<String, AttributeValue> item : result.getItems()) {
+				cellIdsList.add(item.get("timestamp").getN());
+			}
+
+			lastKeyEvaluated = result.getLastEvaluatedKey();
+		} while (lastKeyEvaluated != null);
+
 		return RequestID.createReplyListCell(reqId, cellIdsList).toString();
 	}
 
@@ -137,47 +155,49 @@ public class DDBReader {
 			return RequestID.createError("XMLError", end, "Impossible to parse this date" + e.getMessage()).toString();
 		}
 
-		
-
 		// Making request
-		Condition condition = new Condition().withComparisonOperator(ComparisonOperator.BETWEEN)
-				.withAttributeValueList(
-						new AttributeValue().withN(tbegin.toString() + "000"), new AttributeValue().withN(tend.toString() + "000"));
-
-		QueryRequest queryRequest = new QueryRequest().withTableName(tableName)
-				.withHashKeyValue(new AttributeValue().withN(cellid.toString())).withRangeKeyCondition(condition)
-				.withAttributesToGet(Arrays.asList("flags", "carType", "data"));
-
-		QueryResult result = dynamoDB.query(queryRequest);
-		LOGGER.log(Level.INFO, "Result from request : " + result);
-
+		Key lastKeyEvaluated = null;
 		Map<String, CellDirection> listSpeedside1 = new HashMap<String, CellDirection>();
 		Map<String, CellDirection> listSpeedside2 = new HashMap<String, CellDirection>();
-		for (Map<String, AttributeValue> item : result.getItems()) {
-			if (item.get("flags").getB().get() == 0) {
-				CellDirection cd = listSpeedside1.get(item.get("carType").getN());
-				if (cd == null) {
-					cd = new CellDirection(item.get("carType").getN());
-					listSpeedside1.put(item.get("carType").getN(), cd);
+		Condition condition = new Condition().withComparisonOperator(ComparisonOperator.BETWEEN)
+				.withAttributeValueList(new AttributeValue().withN(tbegin.toString() + "000"),
+						new AttributeValue().withN(tend.toString() + "000"));
+		do {
+			QueryRequest queryRequest = new QueryRequest().withTableName(tableName)
+					.withHashKeyValue(new AttributeValue().withN(cellid.toString())).withRangeKeyCondition(condition)
+					.withAttributesToGet(Arrays.asList("flags", "carType", "data"))
+					.withExclusiveStartKey(lastKeyEvaluated);
+
+			QueryResult result = dynamoDB.query(queryRequest);
+			LOGGER.log(Level.INFO, "Result from request : " + result);
+
+			for (Map<String, AttributeValue> item : result.getItems()) {
+				if (item.get("flags").getB().get() == 0) {
+					CellDirection cd = listSpeedside1.get(item.get("carType").getN());
+					if (cd == null) {
+						cd = new CellDirection(item.get("carType").getN());
+						listSpeedside1.put(item.get("carType").getN(), cd);
+					}
+					cd.addRawData(item.get("data").getB().array());
+				} else {
+					CellDirection cd = listSpeedside2.get(item.get("carType").getN());
+					if (cd == null) {
+						cd = new CellDirection(item.get("carType").getN());
+						listSpeedside2.put(item.get("carType").getN(), cd);
+					}
+					cd.addRawData(item.get("data").getB().array());
 				}
-				cd.addRawData(item.get("data").getB().array());
-			} else {
-				CellDirection cd = listSpeedside2.get(item.get("carType").getN());
-				if (cd == null) {
-					cd = new CellDirection(item.get("carType").getN());
-					listSpeedside2.put(item.get("carType").getN(), cd);
-				}
-				cd.addRawData(item.get("data").getB().array());
 			}
-		}
+
+			lastKeyEvaluated = result.getLastEvaluatedKey();
+		} while (lastKeyEvaluated != null);
 
 		ArrayList<CellDirection> toCompute1 = new ArrayList<CellDirection>(listSpeedside1.values());
 		ArrayList<CellDirection> toCompute2 = new ArrayList<CellDirection>(listSpeedside2.values());
-		
+
 		new ProcessAll(toCompute2);
 		new ProcessAll(toCompute1);
-		
-		
+
 		return RequestID.createReplyCellStatSpeed(reqId, cellid, begin, end, toCompute1, toCompute2).toString();
 	}
 
